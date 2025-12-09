565a8a95c7edea2bdb961ab2bbd42a5816d28165
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2025-12-09 01:32:44 -0500

    fix: Moved LoggerModule to order-service root

--- apps/order-service/order-domain/order-domain-core/order-domain-core.module.ts
+++ apps/order-service/order-domain/order-domain-core/order-domain-core.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
 import { CommonDomainModule } from 'apps/common/common-domain/common-domain.module';
-import { LoggerModule } from 'nestjs-pino';
 
 @Module({
-  imports: [CommonDomainModule, LoggerModule.forRoot()],
+  imports: [CommonDomainModule],
 })
 export class OrderDomainCoreModule {}
--- apps/order-service/src/order-service.module.ts
+++ apps/order-service/src/order-service.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { OrderDomainModule } from '../order-domain/order-domain.module';
 import { OrderApplicationModule } from '../order-application/order-application.module';
 import { OrderDataAccessModule } from '../order-data-access/order-data-access.module';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
-  imports: [OrderDomainModule, OrderApplicationModule, OrderDataAccessModule],
+  imports: [
+    LoggerModule.forRoot(),
+    OrderDomainModule,
+    OrderApplicationModule,
+    OrderDataAccessModule,
+  ],
 })
 export class OrderServiceModule {}
