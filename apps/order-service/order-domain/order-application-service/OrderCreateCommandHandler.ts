380a1dd1162b7a7726baace7f7fd09be5a3faf2b
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2025-12-09 19:06:04 -0500

    feat: Add OrderCreateHelper - lesson 21

--- apps/order-service/order-domain/order-application-service/OrderCreateCommandHandler.ts
+++ apps/order-service/order-domain/order-application-service/OrderCreateCommandHandler.ts
@@ -1,109 +1,31 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { CreateOrderResponse } from './domain/dto/create/CreateOrderResponse';
 import { CreateOrderCommand } from './domain/dto/create/CreateOrderCommand';
-import type { OrderDomainService } from '../order-domain-core/OrderDomainService';
-import type { OrderRepository } from './domain/ports/output/repository/OrderRepository';
-import type { CustomerRepository } from './domain/ports/output/repository/CustomerRepository';
-import type { RestaurantRepository } from './domain/ports/output/repository/RestaurantRepository';
 import { OrderDataMapper } from './domain/mapper/OrderDataMapper';
-import { DataSource, EntityManager } from 'typeorm';
-import { UUID } from 'node:crypto';
-import { Customer } from '../order-domain-core/entity/Customer';
-import { OrderDomainException } from '../order-domain-core/exception/OrderDomainException';
-import { Restaurant } from '../order-domain-core/entity/Restaurant';
-import { Order } from '../order-domain-core/entity/Order';
+import { OrderCreateHelper } from './OrderCreateHelper';
+import type { OrderCreatedPaymentRequestMessagePublisher } from './domain/ports/output/message/publisher/payment/OrderCreatedPaymentRequest';
 
 @Injectable()
 export class OrderCreateCommandHandler {
   private readonly logger = new Logger(OrderCreateCommandHandler.name);
 
   constructor(
-    private readonly dataSource: DataSource,
-    private readonly orderDomainService: OrderDomainService,
-    private readonly orderRepository: OrderRepository,
-    private readonly customerRepository: CustomerRepository,
-    private readonly restaurantRespository: RestaurantRepository,
     private readonly orderDataMapper: OrderDataMapper,
+    private readonly orderCreateHelper: OrderCreateHelper,
+    private readonly orderCreatedPaymentRequestMessagePublisher: OrderCreatedPaymentRequestMessagePublisher,
   ) {}
 
   public async createOrder(
     createOrderCommand: CreateOrderCommand,
   ): Promise<CreateOrderResponse> {
-    return await this.dataSource.transaction<CreateOrderResponse>(
-      async (manager) => {
-        await this.checkCustomer(manager, createOrderCommand.getCustomerId());
-        const restaurant = await this.checkRestaurant(
-          manager,
-          createOrderCommand,
-        );
-        const order =
-          this.orderDataMapper.createOrderCommandToOrder(createOrderCommand);
-        this.orderDomainService.validateAndInitiateOrder(order, restaurant);
-
-        const orderResult = await this.saveOrder(manager, order);
-        this.logger.log(
-          `Order is created with id: ${orderResult.getId().getValue()}`,
-        );
-        return this.orderDataMapper.orderToCreateOrderResponse(orderResult);
-      },
-    );
-  }
-
-  private async checkRestaurant(
-    manager: EntityManager,
-    createOrderCommand: CreateOrderCommand,
-  ): Promise<Restaurant> {
-    const [restaurantId] =
-      this.orderDataMapper.createOrderCommandToRestaurant(createOrderCommand);
-    const restaurant =
-      await this.restaurantRespository.findRestaurantInformationWithManager(
-        manager,
-        restaurantId,
-      );
-    if (!restaurant) {
-      this.logger.warn(
-        `Could not find restaurant with restaurant id: ${createOrderCommand.getRestaurantId()}`,
-      );
-      throw new OrderDomainException({
-        message: `Could not find restaurant with restaurant id: ${createOrderCommand.getRestaurantId()}`,
-      });
-    }
-    return restaurant;
-  }
-
-  private async checkCustomer(
-    manager: EntityManager,
-    customerId: UUID,
-  ): Promise<Customer> {
-    const customer = await this.customerRepository.findCustomerWithManager(
-      manager,
-      customerId,
-    );
-    if (!customer) {
-      this.logger.warn(
-        `Could not find customer with customer id: ${customerId}`,
-      );
-      throw new OrderDomainException({
-        message: `Could not find customer with customer id: ${customerId}`,
-      });
-    }
-    return customer;
-  }
-
-  private async saveOrder(
-    manager: EntityManager,
-    order: Order,
-  ): Promise<Order> {
-    const orderResult = await this.orderRepository.saveWithManager(
-      manager,
-      order,
-    );
-    if (!orderResult) {
-      throw new OrderDomainException({ message: 'Could not save order!' });
-    }
+    const orderCreatedEvent =
+      await this.orderCreateHelper.persistOrder(createOrderCommand);
     this.logger.log(
-      `Order is saved with id: ${orderResult.getId().toString()}`,
+      `Order is created with id: ${orderCreatedEvent.getOrder().getId().toString()}`,
     );
-    return orderResult;
+    this.orderCreatedPaymentRequestMessagePublisher.publish(orderCreatedEvent);
+    return this.orderDataMapper.orderToCreateOrderResponse(
+      orderCreatedEvent.getOrder(),
+    );
   }
 }
