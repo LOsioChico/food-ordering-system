78f62d6b8b7e64941aba693f6dc29c546a9091a1
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2025-12-09 23:57:49 -0500

    feat: Add missing providers and export injectables on order application service module

--- apps/order-service/order-domain/order-application-service/order-application-service.module.ts
+++ apps/order-service/order-domain/order-application-service/order-application-service.module.ts
@@ -1,8 +1,29 @@
 import { Module } from '@nestjs/common';
 import { OrderDomainCoreModule } from '../order-domain-core/order-domain-core.module';
 import { CommonDomainModule } from 'apps/common/common-domain/common-domain.module';
+import { OrderApplicationServiceImpl } from './domain/OrderApplicationServiceImpl';
+import { OrderCreateCommandHandler } from './domain/OrderCreateCommandHandler';
+import { OrderTrackCommandHandler } from './domain/OrderTrackCommandHandler';
+import { OrderCreateHelper } from './domain/OrderCreateHelper';
+import { OrderDataMapper } from './domain/mapper/OrderDataMapper';
+import { PaymentResponseMessageListenerImpl } from './domain/PaymentResponseMessageListenerImpl';
+import { RestaurantApprovalResponseMessageListenerImpl } from './domain/RestaurantApprovalResponseMessageListenerImpl';
 
 @Module({
   imports: [OrderDomainCoreModule, CommonDomainModule],
+  providers: [
+    OrderApplicationServiceImpl,
+    OrderCreateCommandHandler,
+    OrderTrackCommandHandler,
+    OrderCreateHelper,
+    OrderDataMapper,
+    PaymentResponseMessageListenerImpl,
+    RestaurantApprovalResponseMessageListenerImpl,
+  ],
+  exports: [
+    OrderApplicationServiceImpl,
+    PaymentResponseMessageListenerImpl,
+    RestaurantApprovalResponseMessageListenerImpl,
+  ],
 })
 export class OrderApplicationServiceModule {}
